bcd71c5b6cbc4d8e3c9610267e7711100f5ccc92
leslieyi <leslieyi411@gmail.com>
2021-08-23 17:10:02 -0400

    added component

---
+++ my-app-frontend/src/components/Cart.js
@@ -0,0 +1,7 @@
+function Cart(){
+    return(
+        <h1>From Cart</h1>
+    )
+}
+
+export default Cart;
--- my-app-frontend/src/components/Navbar.js
+++ my-app-frontend/src/components/Navbar.js
@@ -1,9 +1,11 @@
 import { Route, Switch, Link } from "react-router-dom";
 import LogInPage from "./LogInPage"
+import Cart from "./Cart"
 function Navbar() {
     return(
 
        <LogInPage/>
+       <Cart />
     )
 }
 export default Navbar;
